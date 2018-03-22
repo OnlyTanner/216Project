1430028d7754ab10b258a87c0d8d0d012a4557a2
Tanner Lisonbee <lisonbeetanner@gmail.com>
2018-03-22 11:36:55 -0700

    Added gitignore to exclude out and .idea directories.

--- Monopoly2/out/production/Monopoly2/resources/scripts/advance-to-go.lua
+++
@@ -1,6 +0,0 @@
-local player = require("PlayerLuaLibrary")
-local lib = require("LuaLibrary")
-
-player.setPlayerPos(player.currPlayer(), 0)
--- The player passed Go
-player.giveMoney(player.currPlayer(), 200)
